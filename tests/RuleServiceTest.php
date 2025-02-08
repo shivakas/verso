21021d5749fb10f32b28535f8a156d899dbbc206
shivaraj <shiva.june@gmail.com>
2025-02-08 15:47:08 +0100

    add test for finding edge cases

--- tests/RuleServiceTest.php
+++ tests/RuleServiceTest.php
@@ -21,7 +21,27 @@ class RuleServiceTest extends TestCase
         ];
         
         $result = $fizzBuzzGenerator->generate(1, 16);
-        //die($result);
+        $this->assertEquals($expectedOutput, $result);
+    }
+    
+    public function testFizzBuzzEdgeCase()
+    {
+        // Create the rules (Fizz, Buzz and FizzBuzz)
+        $rules = [new FizzRule(), new BuzzRule(), new FizzBuzzRule()];
+        $fizzBuzzGenerator = new RuleService($rules);
+
+        // Test when the range has only one number, e.g., 3
+        $expectedOutput = ['Fizz'];
+        $result = $fizzBuzzGenerator->generate(3, 3);
+        $this->assertEquals($expectedOutput, $result);
+
+        // Test when the range has only one number, e.g., 5
+        $expectedOutput = ['Buzz'];
+        $result = $fizzBuzzGenerator->generate(5, 5);
+        $this->assertEquals($expectedOutput, $result);
+
+        $expectedOutput = ['FizzBuzz'];
+        $result = $fizzBuzzGenerator->generate(start: 90, end: 90);
         $this->assertEquals($expectedOutput, $result);
     }
 }
